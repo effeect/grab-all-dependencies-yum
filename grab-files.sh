8e800d9d3c56254edb2ab0942f837d588c5f81ae
Oliver Dimes <oliver.dimes3@gmail.com>
2023-01-09 05:48:17 +0200

    bash script now uses env variables.

--- grab-files.sh
+++ grab-files.sh
@@ -3,5 +3,7 @@ cd /tmp/install-deps
 
 yum install yum-plugin-downloadonly yum-utils zip -y
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-yum install --downloadonly --downloaddir=/tmp/install-deps docker-ce docker-ce-cli containerd.io docker-compose-plugin
+echo "Variable is"
+echo $FILES
+yum install --downloadonly --downloaddir=/tmp/install-deps $FILES 
 zip install-deps *
--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 import subprocess
 import time
 
-def up_container(vmname):
+def up_container(vmname,application):
     # https://docs.docker.com/engine/reference/commandline/exec/
     subprocess.run(["docker", "pull", vmname])
-    subprocess.Popen(["docker", "run", "--rm", "--name", "temp_container", vmname, "tail" , "-f", "/dev/null"])
+    subprocess.Popen(["docker", "run", "--rm", "-e", f"FILES={application}","--name", "temp_container", vmname, "tail" , "-f", "/dev/null"])
     # Need to create failure condition to run this at the start if there is an issue still 
     time.sleep(10)
     subprocess.run(["docker", "cp", "grab-files.sh", "temp_container:/grab-files.sh"])
+    # Run a task to create a variable for the script
     subprocess.run(["docker", "exec", "temp_container", "/bin/sh" ,"-c", "./grab-files.sh"])
     subprocess.run(["docker" ,"cp" , "temp_container:/tmp/install-deps/install-deps.zip", "install-deps.zip"])
 
     subprocess.run(["docker","kill","temp_container"])
 
-print("Hello World")
-up_container("centos:7.9.2009")
+if __name__ == "__main__":
+    up_container("centos:7.9.2009","docker-ce")
